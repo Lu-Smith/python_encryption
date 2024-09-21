859a3280ed092e2d99aa17d1273ccb7b6e4f2c98
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-21 14:33:46 +0100

    Updated encryption.py - decryption logic

--- encryption.py
+++ encryption.py
@@ -7,8 +7,8 @@ key = chars.copy()
 
 random.shuffle(key)
 
-print(f"chars: {chars}")
-print(f"key: {key}")
+#print(f"chars: {chars}")
+#print(f"key: {key}")
 
 # encrypt
 
@@ -22,4 +22,15 @@ for letter in plain_text:
 print(f"original message: {plain_text}")
 print(f"encrypted message: {cipher_text}")
 
+# decrypt
+
+cipher_text = input("Enter a message to decrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+  index = key.index(letter)
+  plain_text += chars[index]
+  
+print(f"encrypted message: {cipher_text}")
+print(f"decrypted message: {plain_text}")
 
