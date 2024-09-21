1132ce176a857f8a50bd5131bd7987ddc787ae38
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-21 14:09:56 +0100

    Updated encryptipn.py logic

--- encryption.py
+++ encryption.py
@@ -2,7 +2,9 @@ import string
 
 chars = " " + string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
+key = chars.copy()
 
-print(chars)
+print(f"chars: {chars}")
+print(f"key: {key}")
 
 
